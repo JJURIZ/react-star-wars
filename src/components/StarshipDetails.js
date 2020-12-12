75cb899fd0b9bb2c55b4541999e2016ef916d39d
Jeremy Uriz <jjuriz@gmail.com>
2020-12-12 16:02:34 -0500

    [App, Components] basic setup functional

---
+++ src/components/StarshipDetails.js
@@ -0,0 +1,10 @@
+function StarshipDetails() {
+    return(
+        <div>
+        <h1>Details Here</h1>
+
+        </div>
+    )
+}
+
+export default StarshipDetails;
---
+++ src/components/Starships.js
@@ -0,0 +1,9 @@
+function Starships() {
+    return(
+    <>
+        <h1>Starships Here</h1>
+    </>
+    )
+}
+
+export default Starships;
