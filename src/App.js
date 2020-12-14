cf961e7560def991f59377466ea2e9ea1dddd2a3
Jeremy Uriz <jjuriz@gmail.com>
2020-12-13 19:30:50 -0500

    [App] Pulled all ships (after reviewing peer's code)

--- src/App.js
+++ src/App.js
@@ -12,17 +12,25 @@ function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [starships, setStarships] = useState([]);
 
-  const getStarships = () => {
-    axios.get(`https://swapi.dev/api/starships/`).then(
-      (results) => {
-        setIsLoaded(true);
-        setStarships(results.data.results);
-      },
-      (error) => {
-        setIsLoaded(true);
-        setError(error);
+    const getStarships = async function() {
+      setIsLoaded(true);
+      console.log('is this on?')
+    try {
+    const results1 = await axios.get('http://swapi.dev/api/starships/');
+    const list1 = results1.data.results;
+    const results2 = await axios.get('http://swapi.dev/api/starships/?page=2');
+    const list2 = results2.data.results;
+    const results3 = await axios.get('http://swapi.dev/api/starships/?page=3');
+    const list3 = results3.data.results;
+    const results4 = await axios.get('http://swapi.dev/api/starships/?page=4');
+    const list4 = results4.data.results;
+    const masterList = list1.concat(list2, list3, list4);
+    setStarships(masterList);
+    }
+      catch(err) {
+        setIsLoaded(false);
+        setError(err);
       }
-    );
   };
   useEffect(() => {
     getStarships();
